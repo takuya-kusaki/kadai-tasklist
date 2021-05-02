d222f8a22f4e71eae232a88f059ae80042d26a3b
takuya.kusaki <takuya.090219@gmail.com>
2021-05-02 20:24:47 +0000

    kadai login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,22 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  
   def index
-    @tasks = Task.all
-  end
-  
-  def show
-    @task = Task.find(params[:id])
-  end
-  
-  def new
-    @task = Task.new
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
   
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = "Task が正常に作成されました"
-      redirect_to @task
+      flash[:success] = "正常に作成されました"
+      redirect_to root_url
     else
-      flash.now[:danger] = "Task が作成されませんでした"
-      render :new
+      @tasks = current_user.tasks.order(id: :desc)
+      flash.now[:danger] = "作成されませんでした"
+      render "tasks/index"
     end
   end
   
@@ -31,10 +28,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      flash[:success] = '正常に更新されました'
+      redirect_to "/"
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = '更新されませんでした'
       render :edit
     end
   end
@@ -43,8 +40,8 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = '正常に削除されました'
+    redirect_to "/"
   end
 
   private
