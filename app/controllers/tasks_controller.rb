2870397cbdc41ecd72dc25e00516c209247656d1
takuya.kusaki <takuya.090219@gmail.com>
2021-05-03 13:24:14 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :create, :edit, :destroy, :update]
+  before_action :require_user_logged_in, only: [:index, :create, :edit, :update]
+  before_action :correct_user, only: [:destroy]
   
   def index
     if logged_in?
@@ -40,11 +41,9 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
-    flash[:success] = '正常に削除されました'
-    redirect_to "/"
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
 
   private
@@ -52,6 +51,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
